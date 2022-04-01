65a5ece324f806389c71ec0076ae1bcb459a2c98
EC2 Default User <ec2-user@ip-172-31-44-23.ap-northeast-1.compute.internal>
2022-04-01 06:52:46 +0000

    set up bootstrap4

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function () { 
     return view('welcome');
 });
+
+  Route::resource('posts', 'PostController');
+ 
+  if (env('APP_ENV') === 'local') {
+     URL::forceScheme('https');
+  } 
